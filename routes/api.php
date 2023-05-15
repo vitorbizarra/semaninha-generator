2e6a096f2f8d4b554097e9a862bb8c3c2fcd11c5
Vitor <vitorbizarracontato@gmail.com>
2023-05-15 14:10:09 -0300

    :construction: First api route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Barryvanveen\Lastfm\Lastfm;
+use GuzzleHttp\Client;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('/', function () {
+    $lastfm = new Lastfm(new Client(), getenv('LASTFM_API_KEY'));
+
+    $albums = $lastfm->userTopAlbums('vitor_bizarra')->get();
+    return response()->json($albums);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
